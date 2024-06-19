3de01df9c731f61643c40ba4386ddf8c4de5b452
tonydocafe <155020337+tonydocafe@users.noreply.github.com>
2024-06-19 19:23:43 -0300

    Update soma_do_maior.c

--- soma_do_maior.c
+++ soma_do_maior.c
@@ -1,23 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-int intervalo(int maior,int menor){
-	if(maior == menor){
-		return menor;
-	}else{
-		return maior + intervalo(maior -1, menor);
-	}
+int intervalo(int maior, int menor) {
+    if (maior == menor) {
+        return menor;
+    } else {
+        return maior + intervalo(maior - 1, menor);
+    }
 }
 
-int main (){
-	int maior,menor;
-	printf("digite o maior numero\n");
-	scanf("%i",&maior);
-	printf("digite o menor numero\n");
-	scanf("%i",&menor);
-	printf("a soma do intervalo é igual a %i\n",intervalo(maior,menor));
+int main() {
+    int maior, menor;
+    printf("Digite o maior numero\n");
+    scanf("%i", &maior);
+    printf("Digite o menor numero\n");
+    scanf("%i", &menor);
 
-	return 0;
+    if (maior < menor) {
+        printf("Erro: o maior número deve ser maior ou igual ao menor número.\n");
+    } else {
+        printf("A soma do intervalo é igual a %i\n", intervalo(maior, menor));
+    }
 
+    return 0;
 }
-			
