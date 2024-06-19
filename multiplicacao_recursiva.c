e533d700bfb4223a2413178cb678e4b81f789160
tonydocafe <155020337+tonydocafe@users.noreply.github.com>
2024-06-19 18:27:03 -0300

    Update multiplicacao_recursiva.c

--- multiplicacao_recursiva.c
+++ multiplicacao_recursiva.c
@@ -1,23 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-int multiplicar(int multiplicando,int multiplicador){
-	if(multiplicador <= 1){
-		return multiplicando;
-	}else{
-		return multiplicando + multiplicar(multiplicando, multiplicador-1);
-	}
+int multiplicar(int multiplicando, int multiplicador) {
+    if (multiplicador == 0) {
+        return 0;
+    } else if (multiplicador > 0) {
+        return multiplicando + multiplicar(multiplicando, multiplicador - 1);
+    } else { // multiplicador < 0
+        return -multiplicando + multiplicar(multiplicando, multiplicador + 1);
+    }
 }
 
-int main (){
-	int maior,menor;
-	printf("digite um numero\n");
-	scanf("%i",&maior);
-	printf("digite outro numero\n");
-	scanf("%i",&menor);
-	printf("o resultado da multiplicação é : %i\n",multiplicar(maior,menor));
+int main() {
+    int maior, menor;
+    printf("Digite um numero\n");
+    scanf("%i", &maior);
+    printf("Digite outro numero\n");
+    scanf("%i", &menor);
+    printf("O resultado da multiplicação é: %i\n", multiplicar(maior, menor));
 
-	return 0;
-
+    return 0;
 }
-	
